9879aa0536c1827ae4f9749fb190be541b716b0d
GHOST <87672676+GHOSTsama2503@users.noreply.github.com>
2023-01-30 20:40:05 -0500

    try to fix the timeout errors

--- env.py
+++ env.py
@@ -7,3 +7,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 DETA_KEY = os.getenv("DETA_KEY")
 FEED_URL = os.getenv("FEED_URL")
+TASK_INTERVAL = int(os.getenv("TASK_INTERVAL", 15)) * 60
