75d04b249a37eeb6acdd11f46e8363fc8f022d53
GHOSTsama2503 <ghostsama2503@gmail.com>
2023-04-22 12:54:00 -0400

    Removed aiogram and improved performance Now retry until each action is completed

--- env.py
+++ env.py
@@ -8,3 +8,4 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 DETA_KEY = os.getenv("DETA_KEY")
 FEED_URL = os.getenv("FEED_URL")
 TASK_INTERVAL = int(os.getenv("TASK_INTERVAL", 15)) * 60
+RETRY_INTERVAL = int(os.getenv("RETRY_INTERVAL", 5))
