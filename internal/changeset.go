232fd98cc1a8336a115e9fdfc20247e6d4a59be5
GHOSTsama2503 <87672676+GHOSTsama2503@users.noreply.github.com>
2024-06-09 12:25:08 -0400

    🐹 first commit in go

---
+++ internal/changeset.go
@@ -0,0 +1,11 @@
+package internal
+
+type Changeset struct {
+	Id          string
+	Title       string
+	Description string
+	Create      string
+	Modify      string
+	Delete      string
+	Username    string
+}
