2a9b8ebbc0682150212ef9e6d47ec2cb0258178c
GHOSTsama2503 <87672676+GHOSTsama2503@users.noreply.github.com>
2024-06-09 12:25:08 -0400

    [Fix] Empty strings in the environment vars

--- src/env.py
+++ src/env.py
@@ -7,5 +7,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 DETA_KEY = os.getenv("DETA_KEY")
 FEED_URL = os.getenv("FEED_URL")
-TASK_INTERVAL = int(os.getenv("TASK_INTERVAL", 15)) * 60
-RETRY_INTERVAL = int(os.getenv("RETRY_INTERVAL", 5))
+TASK_INTERVAL = int(os.getenv("TASK_INTERVAL") or 15) * 60
+RETRY_INTERVAL = int(os.getenv("RETRY_INTERVAL") or 5)
