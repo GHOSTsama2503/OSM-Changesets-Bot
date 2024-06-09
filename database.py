2ac90b5b4f008fd18ecd422757e7735ca4069618
GHOSTsama2503 <87672676+GHOSTsama2503@users.noreply.github.com>
2024-06-09 12:25:08 -0400

    Removed aiogram and improved performance Now retry until each action is completed

--- database.py
+++ database.py
@@ -1,5 +1,11 @@
-from env import DETA_KEY
+import logging
+
 from deta import Deta
+
+from env import DETA_KEY
+
+
+log = logging.getLogger(__name__)
 
 db = Deta(DETA_KEY)
 changesets = db.Base("changesets")
@@ -10,5 +16,9 @@ def already_parsed(changeset_id: int) -> bool:
     return changeset_id <= latest
 
 
-def update_latest(changeset_id: int):
-    changesets.update({"id": changeset_id}, "latest")
+def update_latest(changeset_id: int) -> bool:
+    try:
+        changesets.update({"id": changeset_id}, "latest")
+        return True
+    except:
+        return False
