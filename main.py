ec6d88a077d98cd1fc603e52b4f49c4419eb7fa1
GHOSTsama2503 <87672676+GHOSTsama2503@users.noreply.github.com>
2024-06-09 12:24:47 -0400

    Removed aiogram and improved performance Now retry until each action is completed

--- main.py
+++ main.py
@@ -1,74 +1,113 @@
-from aiogram import Bot
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiohttp import ClientTimeout
 import asyncio
+import json
+import logging
+from urllib.parse import quote
+
+from aiohttp import ClientSession
+
 import database
 import env
 import feed
 import logger
-import logging
-from urllib.parse import quote
 
-bot = Bot(env.BOT_TOKEN, timeout=ClientTimeout(total=60 * 2))
 log = logging.getLogger(__name__)
 
 
-def create_message(changeset: dict) -> str:
-    text = f"{changeset['title']}\n\n"
-    text += f"{changeset['summary']}\n\n"
-    text += f"{changeset['date']}\n"
-    text += f"🟢 {changeset['create']} | 🟠 {changeset['modify']} | 🔴 {changeset['delete']}"
-    return text
+async def send_message(data: dict) -> bool:
+    data = json.dumps(data)
+
+    async with ClientSession() as session:
+        url = f"https://api.telegram.org/bot{env.BOT_TOKEN}/sendMessage"
+        headers = {"Content-Type": "application/json"}
+
+        try:
+            async with session.post(url=url, data=data, headers=headers) as response:
+                response.raise_for_status()
+                return True
+        except:
+            log.warning("Error sending message", exc_info=True)
+            return False
 
 
-async def send_changeset(changeset: dict):
-
+def create_data(changeset: dict) -> dict:
     changeset_url = f"https://www.openstreetmap.org/changeset/{changeset['id']}"
     user_url = f"https://www.openstreetmap.org/user/{quote(changeset['user'])}"
 
-    achavi_url = f"https://overpass-api.de/achavi/?changeset={changeset['id']}"
+    overpass_url = f"https://overpass-api.de/achavi/?changeset={changeset['id']}"
     osmcha_url = f"https://osmcha.org/changesets/{changeset['id']}"
 
     inline_keyboard = [
-        [InlineKeyboardButton("🌐 Changeset", changeset_url), InlineKeyboardButton("👤 User", user_url)],
-        [InlineKeyboardButton("🌐 OSMCha", osmcha_url), InlineKeyboardButton("🌐 Overpass", achavi_url)]
+        [
+            {"text": "🌐 Changeset", "url": changeset_url},
+            {"text": "👤 User", "url": user_url},
+        ],
+        [
+            {"text": "🌐 OSMCha", "url": osmcha_url},
+            {"text": "🌐 Overpass", "url": overpass_url},
+        ],
     ]
 
-    reply_markup = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
-    message = create_message(changeset)
+    text = f"{changeset['title']}\n\n"
+    text += f"{changeset['summary']}\n\n"
+    text += f"{changeset['date']}\n"
 
-    await bot.send_message(env.CHANNEL_ID, message, reply_markup=reply_markup,
-                           disable_web_page_preview=True)
+    text += "🟢 {create} | 🟠 {modify} | 🔴 {delete}".format(
+        create=changeset.get("create"),
+        modify=changeset.get("modify"),
+        delete=changeset.get("delete"),
+    )
+
+    data = {
+        "chat_id": env.CHANNEL_ID,
+        "text": text,
+        "reply_markup": {"inline_keyboard": inline_keyboard},
+        "disable_web_page_preview": True,
+    }
+
+    return data
 
 
-async def worker():
+async def changesets_handler(changesets: list[dict]):
+    for changeset in changesets:
+        data = create_data(changeset)
+
+        sent = False
+        while not sent:
+            sent = await send_message(data=data)
+
+        updated = False
+        while not updated:
+            updated = database.update_latest(changeset["id"])
+
+        # avoid flood
+        await asyncio.sleep(5)
+
+
+async def main():
+    log.info("Client Started!")
+
     while True:
         try:
             new_changesets = feed.new_changesets(env.FEED_URL)
             log.info(f"New Changesets: {len(new_changesets)}")
 
         except:
-            log.error("Error getting changesets:", exc_info=True)
+            log.error("Error getting new changesets:", exc_info=True)
             new_changesets = []
 
-        # if changesets
-        for changeset in new_changesets:
-            try:
-                await send_changeset(changeset)
-                database.update_latest(changeset["id"])
+        if new_changesets:
+            log.info("Working... :(")
+            await changesets_handler(new_changesets)
+            log.info("Finished! :D")
 
-            except:
-                log.error(f"Error sending changeset, changeset_id={changeset.get('id')}", exc_info=True)
-                break
-
-            await asyncio.sleep(5) # avoid flood
-
+        log.info("Sleeping... zzz...")
         await asyncio.sleep(env.TASK_INTERVAL)
 
 
-async def main():
-    loop = asyncio.get_running_loop()
-    await worker()
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, asyncio.exceptions.CancelledError):
+        pass
+    except:
+        log.error("Unexpected error", exc_info=True)
